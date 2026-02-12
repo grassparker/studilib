d74895f150a881033176248dc6a9f266bf097240
Grassparker <candy20190715@outlook.com>
2026-02-12 21:36:07 +0800

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    target: 'esnext'
+  },
   plugins: [react()],
   resolve: {
     alias: {
